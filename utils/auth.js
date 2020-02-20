71f2ed12c731f281f43b5443e5b131470e79210d
c <a>
2020-02-20 14:08:30 +0700

    refactor auth middleware

--- utils/auth.js
+++ utils/auth.js
@@ -4,25 +4,21 @@ const jwt = require('jsonwebtoken')
 const dayjs = require('dayjs')
 
 module.exports={
-    async isLogin(req,res,next){
-        try {
-            const decoded = await checkAuth(req).catch(e=>{throw e})
+    isLogin:(req,res,next)=>{
+        checkAuth(req,(err,decoded)=>{
+            if(err)return res.status(error.code?error.code:500).send({msg:error.message})
             res.locals={decoded}
             next()
-        } catch (error) {
-            return res.status(error.code?error.code:500).send({msg:error.message})
-        }
+        })
     }
 }
 
-async function checkAuth(req){
-    if (!req.headers.token)throw {code:401,message:'not login'}
-    let temp
+async function checkAuth(req,cb){
+    if (!req.headers.token)cb({code:401,message:'not login'})
     jwt.verify(req.headers.token,config.secret,{issuer:'yourcompanyname'},async function(err,decoded){
-        if(err)throw {code:400,message:'bad request'}
-        const data = await db.user.findOne({where:{id:decoded.id}}).catch(e=>{throw new Error(e)})
-        if(!data || dayjs.unix(decoded.iat).add(7,'day')<dayjs() ||decoded.iat<data.iat) throw {code:401,message:'token expired'}
-        temp=decoded
+        if(err)cb({code:400,message:'bad request'})
+        const data = await db.user.findOne({where:{id:decoded.id}}).catch(e=>{cb(new Error(e))})
+        if(!data || dayjs.unix(decoded.iat).add(7,'day')<dayjs() ||decoded.iat<data.iat) cb({code:401,message:'token expired'})
+        cb(null,decoded)
     })
-    return temp
 }
